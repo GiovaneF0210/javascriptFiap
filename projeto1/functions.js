c18192610a60fe986841227fef394490262d9d44
Logon Aluno <logonaluno@local.com>
2020-03-11 22:32:31 -0300

    exercicios

--- projeto1/functions.js
+++ projeto1/functions.js
@@ -1,4 +1,18 @@
 function terceiraAula(){
-	alert("Hello world");
+	//alert("Hello world");
+	//alert("segundo alert");
+	//document.write("A");
 	
+}
+function functionClick(){
+	alert();
+}
+function ex01(){
+	document.write("primeira frase <br>");
+	document.write("Segunda frase");
+}
+function ex02(){
+	document.write("primeira frase <br>");
+	document.write("Segunda frase");
+	alert("Frase alerta!");
 }
